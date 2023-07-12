63526a97f954aadea0d4467a66cec62bf58fe770
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-07-12 19:55:51 +0300

    KotlinSprint-1-2 simplified multiString to String

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -3,9 +3,6 @@ package lesson_1
 fun main () {
 
     var numberOfOrders: UInt = 75u // количество заказов
-    val thankYouText: String = """
-        Благодарим за покупку!
-        Отличного вам настроения!
-    """.trimIndent() // текст с благодарностью за покупку
+    val thankYouText: String = "Благодарим за покупку! Отличного вам настроения!" // текст с благодарностью за покупку
 
 }
---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,28 @@
+package lesson_1
+
+fun main () {
+
+    var numberOfOrders: UInt = 75u // количество заказов
+    val thankYouText: String = "Благодарим за покупку! Отличного вам настроения!"// текст с благодарностью за покупку
+
+    println("""
+        Количество заказов: 
+            $numberOfOrders
+        Текст с благодарностью за покупку: 
+            $thankYouText
+    """.trimIndent())
+
+    var numberOfEmployees: UInt = 2000u // количество работников
+
+//    println("""
+//        Количество работников:
+//            $numberOfEmployees
+//    """.trimIndent())
+
+    --numberOfEmployees
+
+    println("""
+        Количество работников:
+            $numberOfEmployees
+    """.trimIndent())
+}
