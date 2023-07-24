227136078462c342a0426de8ac6f37a530e09d4b
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-07-24 21:22:57 +0300

    KotlinSprint-3-1 greet the user

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,31 @@
+package lesson_3
+
+//import java.time.LocalDateTime
+//import java.time.format.DateTimeFormatter
+
+fun main() {
+
+//    val userName = "Константин"
+//
+//    val currentDateTime = LocalDateTime.now()
+//    val currentHour = currentDateTime.format(DateTimeFormatter.ofPattern("HH"))
+//    // определяем, сколько сейчас часов в формате String
+//
+//    val currentHourInt = currentHour.toInt() // переводим количество часов в Int
+//
+//    if (currentHourInt > 5 && currentHourInt < 17) {
+//        val welcomeText = "Добрый день, "
+//    } else {
+//        val welcomeText = "Добрый вечер, "
+//    }
+
+    var welcomeText = "Добрый день"
+    val userName = "Константин"
+
+    println("$welcomeText, $userName!")
+
+    welcomeText = "Добрый вечер"
+
+    print("$welcomeText, $userName!")
+
+}
