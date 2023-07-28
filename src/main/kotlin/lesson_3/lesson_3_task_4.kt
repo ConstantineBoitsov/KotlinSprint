79bd16e8a958a70ddf519f685b160621a160deff
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-07-28 18:21:57 +0300

    KotlinSprint-3-5 changed positioning format

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -4,20 +4,24 @@ fun main() {
 
     var fromX = 'E' // char, чтобы применять арифметику
     var fromY = 2
+    var positionStart = fromX.toString() + fromY.toString()
 
     var toX = 'E'
     var toY = 4
+    var positionEnd = toX.toString() + toY.toString()
 
     var turnCounter = 0
 
-    println("$fromX$fromY-$toX$toY;${++turnCounter}")
+    println("$positionStart-$positionEnd;${++turnCounter}")
 
     fromX = 'D'
     fromY = 2
+    positionStart = fromX.toString() + fromY.toString()
 
     toX = fromX
     toY = fromY + 1
+    positionEnd = toX.toString() + toY.toString()
 
-    print("$fromX$fromY-$toX$toY;${++turnCounter}")
+    print("$positionStart-$positionEnd;${++turnCounter}")
 
 }
