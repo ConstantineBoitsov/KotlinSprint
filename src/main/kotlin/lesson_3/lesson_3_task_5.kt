58f28ba1657fe542f61e3b23273f1e3e2944c1b1
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-07-29 11:38:40 +0300

    KotlinSprint-3-5 changed split of string and positioning format

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -4,21 +4,23 @@ fun main() {
 
     val turnLog = "D2-D4;0"
 
-    val parts = turnLog.split(";") // [D2-D4, 0]
+    val parts = turnLog.split("-", ";") // [D2, D4, 0]
 
-    val figureCoordinates = parts[0].split("") // [, D, 2, -, D, 4, ]
+//    val figureCoordinates = parts[0].split("") // [, D, 2, -, D, 4, ]
 
-    val fromX = figureCoordinates[1]
-    val fromY = figureCoordinates[2]
+//    val fromX = figureCoordinates[1]
+//    val fromY = figureCoordinates[2]
+//
+//    val toX = figureCoordinates[4]
+//    val toY = figureCoordinates[5]
 
-    val toX = figureCoordinates[4]
-    val toY = figureCoordinates[5]
-
-    val turnCounter = parts[1]
+    val positionStart = parts[0]
+    val positionEnd = parts[1]
+    val turnCounter = parts[2]
 
     print("""
-        Начальное положение фигуры: $fromX$fromY
-        Конечное положение фигуры: $toX$toY
+        Начальное положение фигуры: $positionStart
+        Конечное положение фигуры: $positionEnd
         Номер хода: $turnCounter
     """.trimIndent())
 
