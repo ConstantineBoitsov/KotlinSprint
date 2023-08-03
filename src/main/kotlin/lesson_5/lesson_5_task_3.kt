2fed1ea16b36444d6cf69f87f1005ce1046780ba
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-03 15:03:58 +0300

    KotlinSprint-5-3 Yeeeeeah! Who won the lottery? I did!

---
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -0,0 +1,33 @@
+package lesson_5
+
+fun main() {
+
+    val lotteryNumberOne = 34
+    val lotteryNumberTwo = 68
+    var numberOfMatches = 0
+
+    print(
+        """
+        Вы участвуете в лотерее! Нужно угадать два числа от 1 до 100.
+        Первая попытка: 
+    """.trimIndent()
+    )
+    val firstTry = readln().toInt()
+    if ((firstTry == lotteryNumberOne) or (firstTry == lotteryNumberTwo)) {
+        numberOfMatches++
+    }
+
+    print("Вторая попытка: ")
+    val secondTry = readln().toInt()
+    if ((secondTry != firstTry) and ((secondTry == lotteryNumberOne) or
+                (secondTry == lotteryNumberTwo))
+    ) {
+        numberOfMatches++
+    }
+
+    when (numberOfMatches) {
+        0 -> print("Неудача! Крутите барабан!")
+        1 -> print("Вы выиграли утешительный приз!")
+        2 -> print("Поздравляем! Вы выиграли главный приз!")
+    }
+}
