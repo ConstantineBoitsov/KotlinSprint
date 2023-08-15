1a2712ae7475bafcae6e8fb9893118352f8e3d30
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-15 19:14:45 +0300

    KotlinSprint-7-1 — six-digit password generator

---
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -0,0 +1,15 @@
+package lesson_7
+
+fun main() {
+
+    val symbolOne = ('a'..'z').random()
+    val symbolTwo = (1..9).random().toString()
+    val symbolThree = ('a'..'z').random()
+    val symbolFour = (1..9).random().toString()
+    val symbolFive = ('a'..'z').random()
+    val symbolSix = (1..9).random().toString()
+
+    println("Сгенерированный пароль: ${symbolOne + symbolTwo + 
+        symbolThree + symbolFour + symbolFive + symbolSix}")
+
+}
