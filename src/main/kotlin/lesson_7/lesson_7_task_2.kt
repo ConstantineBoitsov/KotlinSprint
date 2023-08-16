b32048b9a3024d22eed0675f51d9ae19921d0ca6
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-16 09:34:52 +0300

    KotlinSprint-7-2 while → do-while

--- src/main/kotlin/lesson_7/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -2,25 +2,13 @@ package lesson_7
 
 fun main() {
 
-    var verificationCode = (1000..9999).random()
-    println("Ваш код авторизации: $verificationCode")
+    do {
+        val verificationCode = (1000..9999).random()
+        println("\nВаш код авторизации: $verificationCode")
 
-    print("\nВведите полученный код: ")
-    var typedCode = readln().toInt()
-
-    while (typedCode != verificationCode) {
-        verificationCode = (1000..9999).random()
-        print(
-            """
-                
-            Код введён неверно.
-            
-            Ваш новый код авторизации: $verificationCode
-            
-            Введите полученный код: """.trimIndent()
-        )
-        typedCode = readln().toInt()
-    }
+        print("\nВведите полученный код: ")
+        val typedCode = readln().toInt()
+    } while (typedCode != verificationCode)
 
     print("\nДобро пожаловать!")
 
