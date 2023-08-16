4b99c9acf874a0a709f73ec0ff8f87f5edea9506
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-16 10:58:52 +0300

    KotlinSprint-7-5 — generator of password with custom length

---
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -0,0 +1,18 @@
+package lesson_7
+
+fun main() {
+
+    val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    print("Введите длину пароля, который надо сгенерировать: ")
+    val passwordLength = readln().toInt()
+
+    var generatedPassword = ""
+
+    repeat(passwordLength) {
+        generatedPassword += charPool.random()
+    }
+
+    print(generatedPassword)
+
+}
