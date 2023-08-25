49274622f3d95dfbd31b048e0111d8214b4242b0
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-25 19:20:19 +0300

    KotlinSprint-8-5 – array with custom size and content

---
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -0,0 +1,15 @@
+package lesson_8
+
+fun main() {
+
+    print("Сколько будет ингредиентов? ")
+    val ingredientsArray = Array<String>(readln().toInt()) {_ -> ""}
+
+    for (i in ingredientsArray.indices) {
+        print("Введите ${i+1} ингредиент: ")
+        ingredientsArray[i] = readln()
+    }
+
+    print("Список ингредиентов: ${ingredientsArray.joinToString(", ")}.")
+
+}
