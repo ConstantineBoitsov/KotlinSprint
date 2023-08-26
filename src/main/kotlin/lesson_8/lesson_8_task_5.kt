2e1c5f968752e07b7283c7ce078f4e795ea1202e
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-26 12:07:45 +0300

    KotlinSprint-8-5 – chenged «{_ -> ""}» to «{""}»

--- src/main/kotlin/lesson_8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -3,7 +3,7 @@ package lesson_8
 fun main() {
 
     print("Сколько будет ингредиентов? ")
-    val ingredientsArray = Array<String>(readln().toInt()) {_ -> ""}
+    val ingredientsArray = Array<String>(readln().toInt()) {""}
 
     for (i in ingredientsArray.indices) {
         print("Введите ${i+1} ингредиент: ")
