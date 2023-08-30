fe0efe7e223449c308d65928e7a21e13dd78f5de
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-30 13:14:54 +0300

    KotlinSprint-8-4 – placed everything in the cycle

--- src/main/kotlin/lesson_8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_4.kt
@@ -6,24 +6,23 @@ fun main() {
 
     println("Ингредиенты весёлого порошка: ${theFunPowderRecipe.joinToString(", ", "", ".")}")
 
-    print("Какой ингредиент вы хотели бы заменить? ")
     var ingredientToSwap: String
 
     do {
+        print("Какой ингредиент вы хотели бы заменить? ")
         ingredientToSwap = readln()
+
+        if (ingredientToSwap in theFunPowderRecipe) {
+            print("На что его заменить? ")
+            theFunPowderRecipe[theFunPowderRecipe.indexOf(ingredientToSwap)] = readln()
+            println(
+                "Готово!\nВы сохранили следующий список: " +
+                        theFunPowderRecipe.joinToString(", ", "", ".")
+            )
+            return
+        } else {
+            println("Такого ингредиента нет, попробуйте ещё раз.")
+        }
     } while (ingredientToSwap !in theFunPowderRecipe)
-
-//    print("Какой ингредиент вы хотели бы зменить? ")
-//    var ingredientToSwap = readln()
-
-//    while (ingredientToSwap !in theFunPowderRecipe) {
-//        print("Такого ингредиента нет, попробуйте ещё раз. ")
-//        ingredientToSwap = readln()
-//    }
-
-    print("На что его заменить? ")
-    theFunPowderRecipe[theFunPowderRecipe.indexOf(ingredientToSwap)] = readln()
-
-    println("Готово!\nВы сохранили следующий список: ${theFunPowderRecipe.joinToString(", ", "", ".")}")
 
 }
