5d27b7cc5ae948fcc1998f763525e59f567542a1
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-30 18:20:08 +0300

    KotlinSprint-9-2 – adding new ingredients to the list

---
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -0,0 +1,18 @@
+package lesson_9
+
+fun main() {
+
+    val funPowderRecipe: MutableList<String> = mutableListOf("сироп вишнёвый", "смешное слово", "шоколадка")
+
+    println("В рецепте есть базовые ингредиенты: ${funPowderRecipe.joinToString("\n", "\n")}")
+
+    print("Желаете добавить ещё? ")
+    val desireToAdd = readln()
+    if ((desireToAdd == "да") or (desireToAdd == "Да")) {
+        print("Какой ингредиент вы хотите добавить? ")
+        funPowderRecipe.add(readln())
+    } else return
+
+    println("\nТеперь в рецепте есть следующие ингредиенты: ${funPowderRecipe.joinToString("\n", "\n")}")
+
+}
