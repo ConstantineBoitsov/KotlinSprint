a68cc1a69411ea66458c2a166c60bea4c50af39b
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-08-30 16:59:45 +0300

    KotlinSprint-9-1 – introducing the List of ingredients

--- src/main/kotlin/lesson_9/lesson_9_task_1.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -1,2 +1,13 @@
 package lesson_9
 
+fun main() {
+
+    val theFunPowderRecipe: List<String> =
+        listOf("сироп вишнёвый", "смешное слово", "шоколадка", "две шутки", "загадка")
+
+    println("В рецепте есть следующие ингредиенты:")
+    theFunPowderRecipe.forEach {
+        println(it)
+    }
+
+}
