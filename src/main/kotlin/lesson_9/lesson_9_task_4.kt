55776b5798da96686ce8c020b363fd5b6d944a11
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-09-01 19:31:31 +0300

    KotlinSprint-9-4 — sorting the list of 5 ingredients in alphabetical order

---
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -0,0 +1,25 @@
+package lesson_9
+
+fun main() {
+
+    var printedIngredients: String
+    var ingredientsList: List<String>
+
+//    АААААА РЕГУЛЯРНЫЕ ВЫРАЖЕНИЯ
+//    val ingredientsList: List<String> = listOf(*printedIngredients.split("\\s*,\\s*".toRegex()).toTypedArray())
+
+    do {
+        print("Введите 5 ингредиентов блюда через запятую: ")
+        printedIngredients = readln()
+        ingredientsList = listOf(*printedIngredients.split(", ").toTypedArray())
+
+        if (ingredientsList.size == 5) {
+            val sortedList = ingredientsList.sorted().joinToString(", ")
+            println("Список ингредиентов по алфавиту: $sortedList")
+        } else {
+            println("Некорректный ввод")
+        }
+
+    } while (ingredientsList.size != 5)
+
+}
