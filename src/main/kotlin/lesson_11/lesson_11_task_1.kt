ea94fa2c72a42fa707ea03894c812bf8a36b7ba9
ConstantineBoitsov <nyankostnyan@gmail.com>
2023-09-17 20:23:05 +0300

    KotlinSprint-11-1 — print user info

--- src/main/kotlin/lesson_11/lesson_11_task_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_1.kt
@@ -1,2 +1,41 @@
 package lesson_11
 
+fun main() {
+
+    val user1 = User(
+        id = 123,
+        login = "DoomSlayer",
+        password = "secretword",
+        email = "bigbusiness@gmail.com"
+    )
+    val user2 = User(
+        13,
+        "agent007",
+        "trustno1",
+        "bondjamesbond@pisem.net"
+    )
+
+    printUserData(userNumber = 1, user1)
+    printUserData(userNumber = 2, user2)
+
+}
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+) {}
+
+fun printUserData(userNumber: Int, user: User) {
+    println(
+        """
+        User №$userNumber:
+        id: ${user.id}
+        login: ${user.login}
+        password: ${user.password}
+        email: ${user.email}
+        
+    """.trimIndent()
+    )
+}
