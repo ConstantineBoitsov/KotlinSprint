018b81db1c8de28fe7e009dc07486ca268440249
ConstantineBoitsov <nyankostnyan@gmail.com>
2024-01-21 02:21:48 +0300

    KotlinSprint-14-1 — ships hierarchy

--- src/main/kotlin/lesson_14/lesson_14_task_1.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_1.kt
@@ -1,2 +1,33 @@
 package lesson_14
 
+fun main() {
+
+    val linerShip = LinerShip(40, 100, 100)
+    val cargoShip = CargoShip(30, 500, linerShip.maxPassengers)
+    val icebreakerShip = IcebreakerShip(30, linerShip.carryingCapacity, 50, iceBreaking = true)
+
+}
+
+open class LinerShip(
+    val speed: Int,
+    val carryingCapacity: Int,
+    val maxPassengers: Int,
+    val iceBreaking: Boolean = false,
+)
+
+class CargoShip(
+    speed: Int,
+    carryingCapacity: Int,
+    maxPassengers: Int,
+) : LinerShip(speed, carryingCapacity, maxPassengers) {
+
+}
+
+class IcebreakerShip(
+    speed: Int,
+    carryingCapacity: Int,
+    maxPassengers: Int,
+    iceBreaking: Boolean,
+) : LinerShip(speed, carryingCapacity, maxPassengers, iceBreaking = true) {
+
+}
