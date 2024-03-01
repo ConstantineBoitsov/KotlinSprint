36a3859446bf807f16678e03d89b1ff7ac5976cd
ConstantineBoitsov <nyankostnyan@gmail.com>
2024-03-01 22:57:27 +0300

    KotlinSprint-14-5 — finally it is done

--- src/main/kotlin/lesson_14/lesson_14_task_5.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_5.kt
@@ -4,38 +4,53 @@ fun main() {
 
     val theGreatChat = Chat()
 
-    theGreatChat.addMessage(text = "Всем привет в этом чятике!", author = "Ryan Gosling")
+    val message1 = theGreatChat.addMessage(text = "Всем привет в этом чятике!", author = "Ryan Gosling")
     theGreatChat.addThreadMessage(text = "Привет, Райан Гослинг!", author = "Kevin Smith",
-        parentMessageId = (theGreatChat.currentMessageId - 1))
+        parentMessageId = message1.messageId)
 
-    theGreatChat.addMessage("Hello everynyan!", "Alien cat")
-    theGreatChat.addThreadMessage("Oh my gooood!", "Human", theGreatChat.currentMessageId - 1)
-    theGreatChat.addThreadMessage("How are you? Thank you!", "Alien cat", theGreatChat.currentMessageId - 1)
+    val message2 = theGreatChat.addMessage("Hello everynyan!", "Alien cat")
+    theGreatChat.addThreadMessage("Oh my gooood!", "Human", message2.messageId)
+    theGreatChat.addThreadMessage("How are you? Thank you!", "Alien cat", message2.messageId)
 
-    val groupedMessages = theGreatChat.messageMutableList.groupBy { it.messageId }
-    val groupedThreadMessages = theGreatChat.messageMutableList.groupBy { it } // НЕПОНЯТНО
+    theGreatChat.printChat()
 
 }
 
-class Chat() {
+class Chat {
 
-    val messageMutableList = mutableListOf<Message>()
-    var currentMessageId: Int = 1
+    val messageList = mutableListOf<Message>()
+    var currentMessageId: Int = 0
 
-    fun addMessage(text: String, author: String) {
-        messageMutableList.add(
-            Message(messageId = currentMessageId++, authorName = author, messageText = text)
-        )
+    fun addMessage(text: String, author: String) : Message {
+
+        val message = Message(messageId = currentMessageId++, authorName = author, messageText = text)
+        messageList.add(message)
+
+        return message
     }
 
-    fun addThreadMessage(text: String, author: String, parentMessageId: Int) {
-        messageMutableList.add(
-            ChildMessage(messageId = currentMessageId++, authorName = author, messageText = text,
-                parentMessageId = parentMessageId)
-        )
+    fun addThreadMessage(text: String, author: String, parentMessageId: Int) : ChildMessage {
+
+        val childMessage = ChildMessage(messageId = currentMessageId++, authorName = author, messageText = text,
+            parentMessageId = parentMessageId)
+        messageList.add(childMessage)
+
+        return childMessage
     }
 
     fun printChat() {
+
+        val groupedMessagesMap: Map<Int, List<Message>> = messageList.groupBy {
+            if (it is ChildMessage) it.parentMessageId
+            else it.messageId
+        }
+
+        groupedMessagesMap.forEach {(_, messageList) ->
+            messageList.forEach { message ->
+                val tab = if (message is ChildMessage) "\t" else ""
+                println("$tab${message.authorName}: ${message.messageText}")
+            }
+        }
 
     }
 }
