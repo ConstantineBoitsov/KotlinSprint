71d6bf0907f72547d7c4a3826363444543a42349
ConstantineBoitsov <nyankostnyan@gmail.com>
2024-03-23 23:35:10 +0300

    KotlinSprint-16-1 — dice with a secret roll result

--- src/main/kotlin/lesson_16/lesson_16_task_1.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_1.kt
@@ -1,3 +1,17 @@
 package lesson_16
 
+fun main() {
 
+    val dice1 = Dice()
+
+    println(dice1.getRollResult())
+
+}
+
+class Dice {
+
+    private val rollResult = (1..6).random()
+
+    fun getRollResult() = rollResult
+
+}
