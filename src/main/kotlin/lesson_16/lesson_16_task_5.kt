32dceb6b444f7d7cca13e2988dfc68d2a44b9e82
ConstantineBoitsov <nyankostnyan@gmail.com>
2024-03-31 22:51:33 +0300

    KotlinSprint-16-5 — refactoring

--- src/main/kotlin/lesson_16/lesson_16_task_5.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_5.kt
@@ -7,12 +7,6 @@ fun main() {
 
     println("${player1.name} vs ${player2.name}!")
 
-//    while (!player1.isDead() or !player2.isDead()) {
-//
-//        Thread.sleep(500)
-//
-//    }
-
     player1.attack(player2)
     player2.attack(player1)
     player1.heal()
@@ -31,21 +25,18 @@ class Player(
     private var maxHealth: Int,
     private var hitDamage: Int,
 
-) {
+    ) {
 
     private var currentHealth = maxHealth
     private var healingPower = 20
 
     fun attack(opponent: Player) {
-
         opponent.currentHealth -= hitDamage
         println("$name атаковал ${opponent.name} на $hitDamage очков урона")
         opponent.isDead()
-
     }
 
     fun heal() {
-
         val healthHealed: Int
 
         // Нельзя вылечиться выше максимума очков здоровья
@@ -54,8 +45,7 @@ class Player(
             currentHealth += healingPower
             healthHealed = healingPower
 
-        }
-        else {
+        } else {
 
             healthHealed = maxHealth - currentHealth
             currentHealth = maxHealth
@@ -63,25 +53,20 @@ class Player(
         }
 
         println("$name восстановил $healthHealed очков здоровья")
-
     }
 
-    fun isDead() : Boolean {
-
+    private fun isDead(): Boolean {
         return if (currentHealth <= 0) {
             deathOfPlayer()
             true
         } else false
-
     }
 
     private fun deathOfPlayer() {
-
         currentHealth = 0
         hitDamage = 0
         healingPower = 0
         println("$name lost this battle")
-
     }
 
 }
