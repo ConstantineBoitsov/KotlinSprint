dabceea67ce4df8e396e18d807ba31607a924c01
ConstantineBoitsov <nyankostnyan@gmail.com>
2024-04-04 02:00:25 +0300

    KotlinSprint-17-1 — quiz elements

--- src/main/kotlin/lesson_17/lesson_17_task_1.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_1.kt
@@ -1,2 +1,14 @@
 package lesson_17
 
+class QuizItem(_quizQuestion: String, _quizAnswer: String) {
+
+    val quizQuestion = _quizQuestion
+        get() = field
+
+    var quizAnswer = _quizAnswer
+        get() = field
+        set(value) {
+            field = value
+        }
+
+}
