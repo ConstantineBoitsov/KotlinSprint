62d31df9ff6efcd9a342c22ed20600e612c88199
ConstantineBoitsov <nyankostnyan@gmail.com>
2024-06-23 16:51:36 +0300

    KotlinSprint-21-1 — vowel counting

--- src/main/kotlin/lesson_21/lesson_21_task_1.kt
+++ src/main/kotlin/lesson_21/lesson_21_task_1.kt
@@ -1,2 +1,19 @@
 package lesson_21
 
+fun main() {
+
+    val stringWithVowels = "How many vowels are here?"
+    println(stringWithVowels.vowelCount())
+
+}
+
+fun String.vowelCount(): Int {
+    var vowelCounter = 0
+    val vowelsList: List<Char> = listOf('e', 'y', 'u', 'i', 'o', 'a')
+
+    for (element in this) {
+        if (vowelsList.contains(element.lowercaseChar())) vowelCounter++
+    }
+
+    return vowelCounter
+}
